17c11ed9170311341766e8f132e3ee3d64a3eb06
sangjun2 <sj27294626@gmail.com>
2018-02-12 23:32:52 +0900

    DayPicker.java and CreateScheduleActivity.java modified

--- app/src/main/java/com/sang/peoplan/CreateScheduleActivity.java
+++ app/src/main/java/com/sang/peoplan/CreateScheduleActivity.java
@@ -2,6 +2,7 @@ package com.sang.peoplan;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -18,7 +19,10 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
+
+import java.text.SimpleDateFormat;
 
 public class CreateScheduleActivity extends AppCompatActivity {
     Switch isByDay;
@@ -107,6 +111,8 @@ public class CreateScheduleActivity extends AppCompatActivity {
                     weekdaysGroup.setVisibility(View.GONE);
                     timeGroup.setVisibility(View.VISIBLE);
                 }
+
+                isAllDay.setChecked(true);
             }
         });
 
@@ -114,16 +120,28 @@ public class CreateScheduleActivity extends AppCompatActivity {
         isAllDay.setOnCheckedChangeListener(new Switch.OnCheckedChangeListener(){
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                removeViews();
+
+                eventStartText.setText(day);
+                eventEndText.setText(day);
+
+                /*
                 if(b){ // 하루종일
                     eventStartText.setText(day);
                     eventEndText.setText(day);
                 }
                 else{ // not 하루종일
                     LocalTime localTime = new LocalTime();
-                    int hour = localTime.getHourOfDay();
-                    eventStartText.setText(day + " " + String.valueOf(hour) + ":00");
-                    eventEndText.setText(day + " " + String.valueOf(hour + 1) + ":00");
+                    int hour = localTime.getHourOfDay() + 1;
+                    if(hour < 12) {
+                        eventStartText.setText(day + " " + "오전 " + String.valueOf(hour) + ":00");
+                        eventEndText.setText(day + " " + "오전 " + String.valueOf(hour + 1) + ":00");
+                    } else {
+                        eventStartText.setText(day + " " + "오후 " + String.valueOf(hour - 12) + ":00");
+                        eventEndText.setText(day + " " + "오후 " + String.valueOf(hour + 1 - 12) + ":00");
+                    }
                 }
+                */
             }
         });
 
@@ -138,7 +156,16 @@ public class CreateScheduleActivity extends AppCompatActivity {
         eventStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                eventStartNumberPicker.initView(day);
+                if(eventStartNumberPicker.getChildCount() == 0) {
+                    removePickerView(view);
+                    if(isAllDay.isChecked()) {
+                        eventStartNumberPicker.initView(day);
+                    } else {
+                        eventStartNumberPicker.initView(day, new LocalTime().getHourOfDay() + 1);
+                    }
+                } else {
+                    eventStartNumberPicker.removeAllViews();
+                }
             }
         });
 
@@ -146,16 +173,28 @@ public class CreateScheduleActivity extends AppCompatActivity {
         eventEnd.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                eventEndNumberPicker.initView(day);
+                if(eventEndNumberPicker.getChildCount() == 0) {
+                    removePickerView(view);
+                    if(isAllDay.isChecked()) {
+                        eventEndNumberPicker.initView(day);
+                    } else {
+                        eventEndNumberPicker.initView(day, new LocalTime().getHourOfDay() + 2);
+                    }
+                } else {
+                    eventEndNumberPicker.removeAllViews();
+                }
             }
         });
 
         repeatView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                removePickerView(view);
                 Intent intent = new Intent(getApplicationContext(), SetEventRepeatActivity.class);
                 startActivityForResult(intent, REQUESTCODE_REPEAT);
                 overridePendingTransition(R.anim.anim_slide_in_right, R.anim.anim_slide_out_left);
+
+
             }
         });
 
@@ -170,11 +209,116 @@ public class CreateScheduleActivity extends AppCompatActivity {
         confirm.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(eventTitle.getText().equals("")){
-                    Toast.makeText(getApplicationContext(), "제목을 입력해주세요.", Toast.LENGTH_SHORT).show();
+                if(isExistAllData()) {
+                    //db 등록
                 }
             }
         });
+    }
+
+    private boolean isExistAllData() {
+        if(eventTitle.getText().toString().equals("")) {
+            Toast.makeText(getApplicationContext(), "제목을 입력해주세요.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        String[] eventStartTexts = eventStartText.getText().toString().split(" ");
+        String[] eventEndTexts = eventEndText.getText().toString().split(" ");
+
+        String[] startDates = eventStartTexts[0].split("\\.");
+        LocalDate startDate = new LocalDate(Integer.parseInt(startDates[0]), Integer.parseInt(startDates[1]), Integer.parseInt(startDates[2]));
+
+        String[] endDates = eventEndTexts[0].split("\\.");
+        LocalDate endDate = new LocalDate(Integer.parseInt(endDates[0]), Integer.parseInt(endDates[1]), Integer.parseInt(endDates[2]));
+
+        if(startDate.toDate().getTime() > endDate.toDate().getTime()) {
+            Toast.makeText(getApplicationContext(), "종료시간이 시작시간보다 빠릅니다.", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(!isAllDay.isChecked()) {
+            String startType = eventStartTexts[1];
+            String endType = eventEndTexts[1];
+
+            int startHour = Integer.parseInt(eventStartTexts[2].split(":")[0]);
+            int startMinute = Integer.parseInt(eventStartTexts[2].split(":")[1]);
+            if(startType.equals("오전")) {
+                if(startHour == 12) {
+                    startHour = 0;
+                }
+            } else {
+                startHour -= 12;
+            }
+
+            int endHour = Integer.parseInt(eventEndTexts[2].split(":")[0]);
+            int endMinute = Integer.parseInt(eventEndTexts[2].split(":")[1]);
+            if(endType.equals("오전")) {
+                if(endHour == 12) {
+                    endHour = 0;
+                }
+            } else {
+                endHour -= 12;
+            }
+
+            DateTime startTime = startDate.toDateTime(new LocalTime(startHour, startMinute));
+            DateTime endTime = endDate.toDateTime(new LocalTime(endHour, endMinute));
+
+            if(startTime.getMillis() > endTime.getMillis()) {
+                Toast.makeText(getApplicationContext(), "종료시간이 시작시간보다 빠릅니다.", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private void removePickerView(View view) {
+        if(view.getId() == R.id.event_start_daypicker) {
+            eventEndNumberPicker.removeAllViews();
+        } else if(view.getId() == R.id.event_end_daypicker) {
+            eventStartNumberPicker.removeAllViews();
+        } else {
+            eventStartNumberPicker.removeAllViews();
+            eventEndNumberPicker.removeAllViews();
+        }
+    }
+
+    private void removeViews() {
+        eventStartNumberPicker.removeAllViews();
+        eventEndNumberPicker.removeAllViews();
+    }
+
+    public void setEventStartText() {
+        StringBuffer buf = new StringBuffer();
+        LocalDate date = eventStartNumberPicker.getPickerDate();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
+        buf.append(simpleDateFormat.format(date.toDate()));
+
+        if(!isAllDay.isChecked()) {
+            int type = eventStartNumberPicker.typePicker.getValue();
+            int hour = eventStartNumberPicker.hourPicker.getValue();
+            int minute = eventStartNumberPicker.minutePicker.getValue();
+
+            buf.append(" " + eventStartNumberPicker.typePicker.getDisplayedValues()[type] + " " + hour + ":" + eventStartNumberPicker.minutePicker.getDisplayedValues()[minute]);
+        }
+
+        eventStartText.setText(buf + "");
+    }
+
+    public void setEventEndText() {
+        StringBuffer buf = new StringBuffer();
+        LocalDate date = eventEndNumberPicker.getPickerDate();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd");
+        buf.append(simpleDateFormat.format(date.toDate()));
+
+        if(!isAllDay.isChecked()) {
+            int type = eventEndNumberPicker.typePicker.getValue();
+            int hour = eventEndNumberPicker.hourPicker.getValue();
+            int minute = eventEndNumberPicker.minutePicker.getValue();
+
+            buf.append(" " + eventEndNumberPicker.typePicker.getDisplayedValues()[type] + " " + hour + ":" + eventEndNumberPicker.minutePicker.getDisplayedValues()[minute]);
+        }
+
+        eventEndText.setText(buf + "");
     }
 
     @Override
