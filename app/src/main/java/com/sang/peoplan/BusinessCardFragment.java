6677b0bf4e1713153e0b5fd76066b5ba3a1921c2
godsangin <leein3293@naver.com>
2018-02-22 21:01:52 +0900

    CreateGroup & BusinessCardFragment implements

--- app/src/main/java/com/sang/peoplan/BusinessCardFragment.java
+++ app/src/main/java/com/sang/peoplan/BusinessCardFragment.java
@@ -1,14 +1,18 @@
 package com.sang.peoplan;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class BusinessCardFragment extends Fragment {
+    ImageView createBusinessCardView;
 
     public BusinessCardFragment() {
         // Required empty public constructor
@@ -30,7 +34,16 @@ public class BusinessCardFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_business_card, container, false);
+        View view = inflater.inflate(R.layout.fragment_business_card, container, false);
+        createBusinessCardView = view.findViewById(R.id.create_businesscard_view);
+        createBusinessCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity().getApplicationContext(), CreateBusinessCardActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 
     @Override
